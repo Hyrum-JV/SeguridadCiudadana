91f52e40a04f2e204a0599a9ddbce06af013c7ae
roddy - <roddydr11@hotmail.com>
2025-09-17 19:57:34 -0500

    se agrego la interfaz de seleccionar contacto sos

---
+++ app/src/main/java/com/example/seguridadciudadana/Config_SeleccionarContacto.kt
@@ -0,0 +1,22 @@
+package com.example.seguridadciudadana
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Config_SeleccionarContacto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_config_seleccionar_contacto)
+
+
+
+
+
+
+
+    }
+}
